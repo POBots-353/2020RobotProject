bfea885f766cf56a794f468c465bdfe765d0f67c
programming353 <58999047+programming353@users.noreply.github.com>
2020-01-30 14:42:42 -0500

    Conveyor CLass
    RG

---
+++ src/main/java/frc/robot/subsystems/OperatorConveyorSubsystem.java
@@ -0,0 +1,32 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2019 FIRST. All Rights Reserved.                             */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package frc.robot.subsystems;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+import frc.robot.Constants;
+import frc.robot.RobotContainer;
+
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkMaxLowLevel.MotorType;
+
+import edu.wpi.first.wpilibj.DigitalInput;
+
+public class OperatorConveyorSubsystem extends SubsystemBase {
+  /**
+   * Creates a new OperatorConveyorSubsystem.
+   */
+  public OperatorConveyorSubsystem() {
+
+  }
+
+  @Override
+  public void periodic() {
+    // This method will be called once per scheduler run
+  }
+}
